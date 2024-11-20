2b50af6e376af68e95dc01a3049a1606e391af28
edu@yuanhau.com <edu@yuanhau.com>
2024-11-20 08:17:15 -0800

    Refactor logging and error handling; add main-dir.py for directory processing

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ if not os.path.exists("log/"):
 log_filename = "run_" + str(int(time.time())) + ".log"
 log_filepath = os.path.join("log/", log_filename)
 logging.basicConfig(filename=log_filepath, level=logging.INFO)
-logger = logging.getLogger(__name__)
+logger = logging.getLogger("Transcoder")
 
 files = os.listdir(input_dir)
 num = 1
@@ -29,15 +29,17 @@ for file in files:
     imgpath = os.path.join(input_dir, file)
     try :
         img = Image.open(imgpath)
-        basename = os.path.splitext(file)[0]
+        basename = os.path.splitext(file)[0] + "_"
         newfile = basename + str(num) + ".png"
         output_p = os.path.join(output_dir, newfile)
         img.save(output_p, format="PNG")
         num += 1
         print(f"已轉換 {file} 到 {newfile}")
+        logger.info(f"Transcoded {file} to {newfile}")
     except Exception as e:
         print(f"轉換失敗 {file} 原因 {e}")
-        logger.error(f"轉換失敗 {file} 原因 {e}") 
+        logger.error(f"Transcode Error: {e} File: {file}") 
 
 
-print("All files converted")
+print("已轉換完成")
+logger.info("Transcode Completed")
