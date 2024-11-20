fb86c3180cbdd7a4846a602ef4d450d547e97192
Howard <hw@hwtw.cc>
2024-11-20 23:08:47 +0800

    Add image conversion functionality from JPG to PNG

--- main.py
+++ main.py
@@ -1,8 +1,21 @@
 from PIL import Image
 import os
 
-input_image_path = "2.jpg"
-image = Image.open(input_image_path)
-print("Image Format: ", image.format)
-print("Image Mode: ", image.mode)
-print("Image size: ", image.size)
+input_dir = "i"
+output_dir = "o"
+
+if not os.path.exists(output_dir):
+    os.makedirs(output_dir)
+
+files = os.listdir(input_dir)
+
+for file in files:
+    imgpath = os.path.join(input_dir, file)
+    img = Image.open(imgpath)
+    newfile = os.path.join(file)[0] + ".png"
+    output_dir = os.path.join(output_dir, newfile)
+    img.save(output_dir, format="PNG")
+
+    print(f"Converted {file} to Format")
+
+print("All files converted")
